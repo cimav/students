f9ea46f39cdd727cfd836b594072ca769ad9ab59
Enrique <enrique.turcott@cimav.edu.mx>
2015-01-08 09:48:24 -0700

    System mailer sapos adjusment

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -1,9 +1,7 @@
 class SystemMailer < ActionMailer::Base
  default :from  => "atencion.posgrado@cimav.edu.mx"
  def notification_email(object)
-   hash       = eval object.content
-   @full_name = hash[:full_name]
-   @email     = hash[:email]
+   @hash       = eval object.content
    @to        = object.to
    mail(:to=> @to,:subject=>object.subject)
  end 
