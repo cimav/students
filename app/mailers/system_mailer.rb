f3bab6f3100bc31be783e30b181e3f7dfec34adc
Enrique <enrique.turcott@cimav.edu.mx>
2016-05-23 15:16:45 -0600

    Add email view options

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -1,7 +1,7 @@
 class SystemMailer < ActionMailer::Base
  default :from  => "atencion.posgrado@cimav.edu.mx"
  def notification_email(object)
-   @hash       = eval object.content
+   @hash      = eval object.content
    @to        = object.to
    mail(:to=> @to,:subject=>object.subject)
  end 
