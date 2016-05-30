cc3840a267f4be0027c5fe4b5cdf4768a0058915
Enrique <enrique.turcott@cimav.edu.mx>
2016-05-30 13:56:34 -0600

    Fix mailer system

--- app/mailers/system_mailer.rb
+++ app/mailers/system_mailer.rb
@@ -3,6 +3,13 @@ class SystemMailer < ActionMailer::Base
  def notification_email(object)
    @hash      = eval object.content
    @to        = object.to
-   mail(:to=> @to,:subject=>object.subject)
+ 
+   @reply_to  = @hash[:reply_to] rescue nil
+
+   if @reply_to.nil?
+     mail(:to=> @to,:subject=>object.subject)
+   else
+     mail(:to=> @to,:subject=>object.subject,:reply_to=>@reply_to)
+   end
  end 
 end
